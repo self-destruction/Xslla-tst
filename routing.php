ae3bdb25665f3943c86e9d5fa0190068cd0e166c
self-destruction <syrchikov_max@mail.ru>
2017-07-14 19:00:48 +0500

    Исправление

--- routing.php
+++ routing.php
@@ -1,36 +1,42 @@
 <?php
-$file = '/' . trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), 'file=');
-$path = __DIR__ . $file;
-if (file_exists($path))
+$file = $_GET['file'];
+if (file_exists($file))
 {
   switch($_SERVER['REQUEST_METHOD'])
   {
     case 'HEAD':
     {
-      header('Content-length: '.filesize($path));
+      header('Content-length: '.filesize($file));
       break;
     }
     case 'GET':
     {
-      echo nl2br(file_get_contents($path));
+      readfile($file);
       break;
     }
     case 'POST':
     {
-      file_put_contents($path, file_get_contents("php://input"));
+      file_put_contents($file, file_get_contents("php://input"));
       break;
     }
     case 'DELETE':
     {
-      unlink($path);
+      unlink($file);
       break;
     }
     case 'PATCH':
     {
-      file_put_contents($path, file_get_contents("php://input"), FILE_APPEND);
+      file_put_contents($file, file_get_contents("php://input"), FILE_APPEND);
+      break;
+    }
+    default:
+    {
       break;
     }
   }
 }
 else
-  exit('File is not found.');
+{
+  http_response_code(500);
+  echo 'File not found.';
+}
